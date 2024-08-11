8e5112085e8bbf93e25d0b7a8a1d8dca05084ea1
Soojin Hwang <soojiny.hwang@gmail.com>
2024-08-11 16:02:02 +0100

    Added some elements and laid out landing page with the grid background at the back

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 
-<<<<<<< HEAD
 // import Button from "./components/Button";
 // import MainHeading from "./components/MainHeading";
-=======
-import Button from "./components/Button";
-import MainHeading from "./components/MainHeading";
-import BackgroundScreen from "./components/BackgroundScreen";
-import gridImage from "./images/grid.png";
->>>>>>> 9a2c02d797e69a8d1756899125de373c242ded5b
-
+// import BackgroundScreen from "./components/BackgroundScreen";
+// import gridImage from "./images/grid.png";
 // import DisplayTeam from "./components/Team";
 // import Navbar from "./components/Navbar";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </Router>
-=======
     <div className="App">
-      <Navbar />
-      <BackgroundScreen url={gridImage} />
-      <MainHeading title="Welcome" />
-      <DisplayTeam />
-      <Button text="Start quiz" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </div>
->>>>>>> 9a2c02d797e69a8d1756899125de373c242ded5b
   );
 }
 
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,14 +1,23 @@
 import React from "react";
+import "./LandingPage.css";
+
 import Navbar from "../components/Navbar";
+import BackgroundScreen from "../components/BackgroundScreen";
 import MainHeading from "../components/MainHeading";
 import Button from "../components/Button";
+import gridImage from "../images/grid.png";
 
 function LandingPage() {
   return (
     <div className="container">
       <Navbar />
-      <MainHeading title="Welcome to" />
-      <Button text="Let's get quizzing!" />
+      <div className="layout-container">
+        <BackgroundScreen url={gridImage} />
+        <div className="content">
+          <MainHeading title="Welcome to" />
+          <Button text="Let's get quizzing!" />
+        </div>
+      </div>
     </div>
   );
 }
