b08d7f01d985953765778309867d6089b45b677b
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-08-13 11:51:57 +0100

    lint errors fixed

--- client/src/components/quiz/FetchQuestion.js
+++ client/src/components/quiz/FetchQuestion.js
@@ -2,21 +2,23 @@ import React from "react";
 import { useState } from "react";
 
 const FetchQuestion = () => {
-    const [questions, setQuestions] = useState(["questions placeholder"]);
-    const performSearch = async () => {
-        const result = await fetch("/api/search/");
-        const jsonResponse = await result.json();
-        setQuestions(jsonResponse);
-    };
+  const [questions, setQuestions] = useState(["questions placeholder"]);
+  const performSearch = async () => {
+    const result = await fetch("/api/search/");
+    const jsonResponse = await result.json();
+    setQuestions(jsonResponse);
+  };
 
-    return (
-        <>
-        <button onClick={performSearch}>Fetch Questions</button>
-        <ul>
-        {questions.map(text => <li key={text}>{text}</li>)}
-        </ul>
-        </>
-    );
+  return (
+    <>
+      <button onClick={performSearch}>Fetch Questions</button>
+      <ul>
+        {questions.map((text) => (
+          <li key={text}>{text}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default FetchQuestion;
