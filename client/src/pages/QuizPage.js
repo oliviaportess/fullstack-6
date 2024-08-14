044a83aa5c49de82d7682e02cd30f4b317e6f36d
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-08-14 15:08:35 +0100

    initial logic and state for running the quiz

--- client/src/pages/QuizPage.js
+++ client/src/pages/QuizPage.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { useState } from "react";
-import useFetchedQuestions from "../hooks/useFetchedQuestions";
+import { useSelector, useDispatch } from "react-redux";
+
+//import useFetchedQuestions from "../hooks/useFetchedQuestions";
+import { quizActions } from "../components/quiz/quizReducer.js";
+import QUESTIONS from "..//data/questions.js";
 
 import Navbar from "../components/Navbar";
 import BackgroundScreen from "../components/BackgroundScreen";
@@ -8,51 +11,67 @@ import gridImage from "../images/grid.png";
 import MainHeading from "../components/MainHeading";
 import Button from "../components/Button";
 import AnswerOptions from "../components/quiz/AnswerOptions";
-
-// import AnswerButton from "../components/AnswerButton";
-// import Question from "../components/quiz/Question";
+import Question from "../components/quiz/Question";
 // import FetchQuestion from "../components/quiz/FetchQuestion";
 
 function QuizPage() {
-  const category = 9; //General Knowledge
-  const difficulty = "easy"; // hard coded for now
-  const url = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`;
+  const dispatch = useDispatch();
+  const activeQuestionIndex = useSelector(
+    (state) => state.quiz.activeQuestionIndex,
+  );
 
-  const { data: questions, loading, error } = useFetchedQuestions(url);
-  // const [currentQuestion, setCurrentQuestion] = useState(questions);
-  const [currentQuestionIndex] = useState(0);
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+  const lastQuestion = activeQuestionIndex === QUESTIONS.length - 1;
+  // const category = 9; //General Knowledge
+  // const difficulty = "easy"; // hard coded for now
+  // const url = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`;
 
-  if (loading) {
-    return <div>Loading...</div>;
+  // const { data: questions, loading, error } = useFetchedQuestions(url);
+  // // const [currentQuestion, setCurrentQuestion] = useState(questions);
+  // const [currentQuestionIndex] = useState(0);
+
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // }
+
+  // // console.log(setCurrentQuestion(questions[0]));
+
+  // const currentQuestion = questions[currentQuestionIndex];
+  // const lastQuestion = questions[questions.length - 1];
+
+  // if (!currentQuestion) {
+  //   return <div>No questions available</div>;
+  // }
+
+  // const answers = [
+  //   currentQuestion.correct_answer,
+  //   ...currentQuestion.incorrect_answers,
+  // ].sort();
+
+  // // console.log(questions);
+  // // console.log(currentQuestion);
+  // // console.log(answers);
+
+  // function handleAnswerClick(answer) {
+  //   console.log(answer);
+  // }
+
+  function handleNextQuestion() {
+    dispatch(quizActions.incrementActiveQuestionIndex());
+    dispatch(quizActions.unansweredAnswerState());
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  if (quizIsComplete) {
+    return (
+      <div>
+        <h2>Will go to score component</h2>
+      </div>
+    );
   }
-
-  // console.log(setCurrentQuestion(questions[0]));
-
-  const currentQuestion = questions[currentQuestionIndex];
-  const lastQuestion = questions[questions.length - 1];
-
-  if (!currentQuestion) {
-    return <div>No questions available</div>;
-  }
-
-  const answers = [
-    currentQuestion.correct_answer,
-    ...currentQuestion.incorrect_answers,
-  ].sort();
-
-  // console.log(questions);
-  // console.log(currentQuestion);
-  // console.log(answers);
-
-  function handleAnswerClick(answer) {
-    console.log(answer);
-  }
-
-  function handleNextQuestion() {}
 
   return (
     <div className="container">
@@ -61,17 +80,14 @@ function QuizPage() {
         <BackgroundScreen url={gridImage} />
         <div className="content">
           <MainHeading
-            title={`${currentQuestionIndex + 1}/${questions.length}`}
+            title={`QUESTION ${activeQuestionIndex + 1}/${QUESTIONS.length}`}
           />
-          <MainHeading
-            className="heading-small"
-            title={`${currentQuestion.question}`}
-          />
+          <Question />
           <div className="answers-container">
-            <AnswerOptions answers={answers} onClick={handleAnswerClick} />
+            <AnswerOptions key={activeQuestionIndex} />
           </div>
           <Button
-            text={currentQuestion === lastQuestion ? "Finish" : "Next"}
+            text={lastQuestion ? "Finish" : "Next"}
             onClick={handleNextQuestion}
           />
         </div>
