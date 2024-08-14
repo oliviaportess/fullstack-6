480a80b711b767c1e87c35b03b61ce2e39ebb614
Soojin Hwang <soojiny.hwang@gmail.com>
2024-08-14 16:39:04 +0100

    Added some content to page

--- client/src/pages/InstructionsPage.js
+++ client/src/pages/InstructionsPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./InstructionsPage.css";
+
 import Navbar from "../components/Navbar";
 import BackgroundScreen from "../components/BackgroundScreen";
 import MainHeading from "../components/MainHeading";
@@ -15,12 +17,35 @@ function InstructionsPage() {
         <BackgroundScreen url={gridImage} />
         <div className="content">
           <MainHeading title="How to Play" />
-          <Link to="/">
-            <Button text="Back" className="grey" />
-          </Link>
-          <Link to="/quiz">
-            <Button text="Generate Quiz" />
-          </Link>
+          <div className="instructions-layout">
+            <div className="instructions">
+              <MainHeading title="Instructions" className="heading-small" />
+              <ol>
+                <li>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Donec a nunc gravida, mollis ipsum in, condimentum erat.
+                  </p>
+                </li>
+                <li>
+                  <p>Nam id justo lacinia, aliquet mauris sed, auctor neque.</p>
+                </li>
+              </ol>
+            </div>
+            <form></form>
+          </div>
+          <div className="nav-links">
+            <Link to="/">
+              <Button text="Back" className="grey" />
+            </Link>
+            <Link to="/quiz">
+              <Button text="Generate Quiz" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
