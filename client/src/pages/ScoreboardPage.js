1acb7fb080f38e901f4cf54b051b5bf77a2cc5a2
raniab1903 <148359712+raniab1903@users.noreply.github.com>
2024-08-20 14:51:25 +0100

    Update ScoreboardPage.js
    incorporated reusable components

--- client/src/pages/ScoreboardPage.js
+++ client/src/pages/ScoreboardPage.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useSelector } from "react-redux";
-import './UserScore.css'; // Import the CSS specific to UserScore
+import './UserScore.css';
 import ScoreList from './ScoreList';
 import UserScoreDisplay from './UserScoreDisplay';
+import Navbar from "../components/Navbar"; // Import Navbar component
+import BackgroundScreen from "../components/BackgroundScreen"; // Import BackgroundScreen component
+import gridImage from "../images/grid.png"; // Import background image
 
 const UserScore = ({ scores, userScore }) => {
-  const score = useSelector((state) => state.quiz.score)
+  const score = useSelector((state) => state.quiz.score);
+
   return (
-    <div className="scoreboard">
-      <h2>Scoreboard</h2>
-      <ScoreList scores={scores} />
-      <UserScoreDisplay userScore={userScore} />
-      <button className="back-button">Back to Home</button>
+    <div className="scoreboard-container">
+      <Navbar /> {/* Add the Navbar for consistency */}
+      <BackgroundScreen url={gridImage} /> {/* Add background styling */}
+      <div className="scoreboard-content">
+        <h2>Scoreboard</h2>
+        <ScoreList scores={scores} />
+        <UserScoreDisplay userScore={userScore} />
+        <button className="back-button">Back to Home</button>
+      </div>
     </div>
   );
 };
