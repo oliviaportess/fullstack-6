581f8c18b2b05fc3214c87e037ec3f871871d992
raniab1903 <148359712+raniab1903@users.noreply.github.com>
2024-08-20 13:42:54 +0100

    Update ScoreboardPage.js
    Fetched scoreboard - g6-86 on jira

--- client/src/pages/ScoreboardPage.js
+++ client/src/pages/ScoreboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 import './UserScore.css'; // Import the CSS specific to UserScore
 import ScoreList from './ScoreList';
 import UserScoreDisplay from './UserScoreDisplay';
 
 const UserScore = ({ scores, userScore }) => {
+  const score = useSelector((state) => state.quiz.score)
   return (
     <div className="scoreboard">
       <h2>Scoreboard</h2>
