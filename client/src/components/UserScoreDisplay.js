2e235e23e5ab210a32de73e907d8ad16fa4b5fd0
Olivia Portess <cm16op@gmail.com>
2024-08-22 15:17:01 +0100

    showing actual score on scoreboard page

--- client/src/components/UserScoreDisplay.js
+++ client/src/components/UserScoreDisplay.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./UserScoreDisplay.css"; // Import the CSS specific to UserScoreDisplay
+import { useSelector } from "react-redux";
 
-const UserScoreDisplay = ({ userScore }) => {
+const UserScoreDisplay = () => {
+  const userScore = useSelector((state) => state.quiz.score)
+  const totalQuestions = useSelector((state) => state.quiz.activeQuestionIndex)
   return (
     <div className="user-score">
       <p>Your Score</p>
-      <p className="score">{userScore}/10</p>
+      <p className="score">{userScore}/{totalQuestions}</p>
     </div>
   );
 };
