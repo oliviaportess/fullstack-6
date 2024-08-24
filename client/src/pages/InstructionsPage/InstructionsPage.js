f8e13d3dcbf7b985ba9242649a8661fd746a93ce
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-08-24 10:37:42 +0100

    Can't go to the instructions page unless you came from the landing page

--- client/src/pages/InstructionsPage/InstructionsPage.js
+++ client/src/pages/InstructionsPage/InstructionsPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useLocation } from "react-router-dom";
 
 import "./InstructionsPage.css";
 
@@ -13,46 +13,52 @@ function InstructionsPage() {
   const questions = useSelector((state) => state.quiz.questions);
   const isFetching = useSelector((state) => state.api.isFetching);
   const isWaiting = useSelector((state) => state.api.isWaiting);
+  const { state } = useLocation();
 
-  return (
-    <>
-      <BrowserTab title="Instructions" />
-      <div className="content">
-        <MainHeading title="How to Play" />
-        <div className="instructions-layout">
-          <div className="instructions">
-            <MainHeading title="Instructions" className="heading-small" />
-            <ol>
-              <li className="list-instructions">
-                Use the form on the right to generate your own quiz
-              </li>
-              <br></br>
-              <li className="list-instructions">
-                Select the number of questions (between 5 and 20), the category,
-                difficulty, and the type of quiz you would like to play
-              </li>
-              <br></br>
-              <li className="list-instructions">
-                Press Submit then Start the Quiz!
-              </li>
-            </ol>
+  if (state && state.fromLandingPage) {
+    return (
+      <>
+        <BrowserTab title="Instructions" />
+        <div className="content">
+          <MainHeading title="How to Play" />
+          <div className="instructions-layout">
+            <div className="instructions">
+              <MainHeading title="Instructions" className="heading-small" />
+              <ol>
+                <li className="list-instructions">
+                  Use the form on the right to generate your own quiz
+                </li>
+                <br></br>
+                <li className="list-instructions">
+                  Select the number of questions (between 5 and 20), the
+                  category, difficulty, and the type of quiz you would like to
+                  play
+                </li>
+                <br></br>
+                <li className="list-instructions">
+                  Press Submit then Start the Quiz!
+                </li>
+              </ol>
+            </div>
+            <QuizForm text={isWaiting ? "Loading..." : "Submit"} />
           </div>
-          <QuizForm text={isWaiting ? "Loading..." : "Submit"} />
         </div>
-      </div>
-      <div className="nav-links">
-        <Link to="/">
-          <Button text="Back" className="grey" />
-        </Link>
-        <Link to="/quiz">
-          <Button
-            text="Start Quiz"
-            isDisabled={questions.length === 0 || isFetching}
-          />
-        </Link>
-      </div>
-    </>
-  );
+        <div className="nav-links">
+          <Link to="/">
+            <Button text="Back" className="grey" />
+          </Link>
+          <Link to="/quiz">
+            <Button
+              text="Start Quiz"
+              isDisabled={questions.length === 0 || isFetching}
+            />
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  return <Navigate to="/" />;
 }
 
 export default InstructionsPage;
